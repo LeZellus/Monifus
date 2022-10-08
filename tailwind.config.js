4aef202ffffd82f40faf06d59d68bfeec4cf606a
Zelller <matheo.zeller@gmail.com>
2022-10-08 05:37:13 +0200

    :lipstick: add google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,42 @@ module.exports = {
                 }
             }
         },
+        fontFamily: {
+            'body': [
+                'Nunito Sans',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            'sans': [
+                'Nunito Sans',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ]
+        }
     },
     plugins: [
         require('flowbite/plugin')
