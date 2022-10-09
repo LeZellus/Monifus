5ad0fa8bfd58cecd648405a3d79cb03357b57f00
Zelller <matheo.zeller@gmail.com>
2022-10-09 20:57:33 +0200

    :sparkles: add api consume service

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\HttpClient\CachingHttpClient;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    private $apiService;
+
+    public function __construct(CallApiService $apiService)
     {
-        return $this->render('home/home.html.twig');
+        $this->apiService = $apiService;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(CacheInterface $cache): Response
+    {
+        $datasCache = $cache->get('datasCache', function (){
+            return $this->apiService->getDofapiData();
+        });
+
+        return $this->render('home/home.html.twig', [
+            "datas" => $datasCache
+        ]);
     }
 }
