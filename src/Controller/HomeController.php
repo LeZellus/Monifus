3efc5ad838b99bc94beb1c00b9cd2b29d9a2239f
Zelller <matheo.zeller@gmail.com>
2022-10-10 16:51:13 +0200

    fix phpstorm bug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,35 +3,20 @@
 namespace App\Controller;
 
 use App\Repository\ResourceRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class HomeController extends AbstractController
 {
-    private $security;
-
-    public function __construct(Security $security)
-    {
-        $this->security = $security;
-    }
-
     #[Route('/', name: 'app_home')]
     public function index(CacheInterface $cache): Response
     {
-        $datasCache = $cache->get('datasCache', function (ResourceRepository $resourceRepository){
-            return $resourceRepository->findAll();
-        });
+//        $datasCache = $cache->get('datasCache', function (ResourceRepository $resourceRepository){
+//            return $resourceRepository->findAll();
+//        });
 
-        if ($this->security->isGranted('ROLE_SALES_ADMIN')) {
-            $salesData['top_secret_numbers'] = rand();
-        }
-
-        return $this->render('home/home.html.twig', [
-            "datas" => $datasCache
-        ]);
+        return $this->render('home/home.html.twig');
     }
 }
---
+++ src/Controller/MonitorController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Monitor;
+use App\Form\MonitorType;
+use App\Repository\MonitorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/monitor')]
+class MonitorController extends AbstractController
+{
+    #[Route('/', name: 'app_monitor_index', methods: ['GET'])]
+    public function index(MonitorRepository $monitorRepository): Response
+    {
+        return $this->render('monitor/index.html.twig', [
+            'monitors' => $monitorRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_monitor_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, MonitorRepository $monitorRepository): Response
+    {
+        $monitor = new Monitor();
+        $form = $this->createForm(MonitorType::class, $monitor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $monitorRepository->save($monitor, true);
+
+            return $this->redirectToRoute('app_monitor_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('monitor/new.html.twig', [
+            'monitor' => $monitor,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_monitor_show', methods: ['GET'])]
+    public function show(Monitor $monitor): Response
+    {
+        return $this->render('monitor/show.html.twig', [
+            'monitor' => $monitor,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_monitor_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Monitor $monitor, MonitorRepository $monitorRepository): Response
+    {
+        $form = $this->createForm(MonitorType::class, $monitor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $monitorRepository->save($monitor, true);
+
+            return $this->redirectToRoute('app_monitor_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('monitor/edit.html.twig', [
+            'monitor' => $monitor,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_monitor_delete', methods: ['POST'])]
+    public function delete(Request $request, Monitor $monitor, MonitorRepository $monitorRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$monitor->getId(), $request->request->get('_token'))) {
+            $monitorRepository->remove($monitor, true);
+        }
+
+        return $this->redirectToRoute('app_monitor_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
