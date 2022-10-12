ec4ad7a9e2d0981c88418792d442c748b94adcbd
Zelller <matheo.zeller@gmail.com>
2022-10-12 13:36:25 +0200

    :sparkles: average feature on monitors

--- src/Form/MonitorType.php
+++ src/Form/MonitorType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Monitor;
+use App\Entity\Resource;
+use App\Entity\Stock;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,20 @@ class MonitorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('price')
-            ->add('resource')
-            ->add('stock')
-            ->add('user')
+            ->add('price', NumberType::class, [
+                'attr' => ["class" => "form-input"],
+                "label_attr" => ["class" => "form-label"]
+            ])
+            ->add('resource', EntityType::class, [
+                'class' => Resource::class,
+                'choice_label' => 'name',
+                'attr' => ["class" => "form-input"]
+            ])
+            ->add('stock', EntityType::class, [
+                'class' => Stock::class,
+                'choice_label' => 'quantity',
+                'attr' => ["class" => "form-input"]
+            ])
         ;
     }
 
