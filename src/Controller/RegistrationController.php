137ebc022b429a5c0aed431c8b397ef1a8f430f2
Mathéo <matheo.zeller@gmail.com>
2024-01-26 20:39:13 +0100

    :sparkles: Roles + fiex

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,7 +37,7 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
+            $user->setRoles(["ROLE_USER"]);
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
