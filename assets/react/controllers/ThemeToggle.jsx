02a068113f74a3e39b4b9f67a383c9296858913c
Mathéo <matheo.zeller@gmail.com>
2024-01-31 20:37:51 +0100

    :lipstick: Fix toggle dark mode

--- assets/react/controllers/ThemeToggle.jsx
+++ assets/react/controllers/ThemeToggle.jsx
@@ -23,12 +23,6 @@ function ThemeToggle() {
                        onChange={toggleTheme} readOnly/>
                 <div
                     className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-300 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full peer-checked:after:border-gray-800 after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-gray-800 after:border-gray-800 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-400"></div>
-                <div className="absolute w-full h-full rounded-full flex px-1" id="darkmode-button">
-                    <i data-feather="sun" id="theme-toggle-light-icon"
-                       className={`text-white w-4 ${!isDarkTheme ? 'hidden' : ''}`}></i>
-                    <i data-feather="moon" id="theme-toggle-dark-icon"
-                       className={`ml-auto text-white w-4 ${isDarkTheme ? 'hidden' : ''}`}></i>
-                </div>
             </label>
         </div>
     );
