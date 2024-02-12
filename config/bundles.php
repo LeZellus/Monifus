4b79a6485f2f4bd05dd2b6a6ac2e7f21f7054908
Mathéo <matheo.zeller@gmail.com>
2024-02-13 00:39:31 +0100

    :lipstick: Error poages

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
