f82c20fc033dc6b65cb9f09450a5157c879d3e70
Mathéo <matheo.zeller@gmail.com>
2024-03-16 23:57:28 +0100

    :heavy_minus_sign: Lazyimages

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,6 @@
 import feather from "feather-icons";
 feather.replace();
 
-import 'lazysizes';
-
 import { registerReactControllerComponents } from '@symfony/ux-react';
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
 
